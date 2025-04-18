880540035350023d81e2cbf4326fcc5ec46b29a5
Arcie <125467182+juliet2112@users.noreply.github.com>
2025-04-18 13:41:42 +0200

    Update zad3_akc1.py

--- grafy/offline3/zad3_akc1.py
+++ grafy/offline3/zad3_akc1.py
@@ -2,7 +2,7 @@ from zad3testy import runtests
 from collections import deque
 from math import inf
 
-def bfs(G,s,t):
+def bfs(G,s,t,mem = (None,None)):
     Q = deque()
     visited = [False for i in range (len(G))]
     d = [inf for i in range (len(G))]
@@ -12,10 +12,11 @@ def bfs(G,s,t):
     while (Q):
         v = Q.popleft()
         for u in G[v]:
-            if visited[u] == False:
-                Q.append(u)
-                visited[u] = True
-                d[u] = d[v]+1
+            if((min(v,u),max(v,u)) != mem):
+                if visited[u] == False:
+                    Q.append(u)
+                    visited[u] = True
+                    d[u] = d[v]+1
     return (d[t])
 
 
@@ -24,13 +25,13 @@ def longer( G, s, t ):
     d = bfs(G,s,t)
     for i in range (len(G)):
         for j in range (len(G[i])):
-            mem = G[i][j]
-            G[i][j] = i
-            dnew = bfs (G,s,t)
-            if (dnew > d): return (i,mem)
-            G[i][j] = mem
+            if( i < G[i][j]):
+                mem = i,G[i][j]
+                dnew = bfs (G,s,t,mem)
+                if (dnew > d): return (mem)
     
     return (None)
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( longer, all_tests = True )
+
