570a0c5c398cd68554bc7ff2f1f0580748c8ae7a
Arcie <125467182+juliet2112@users.noreply.github.com>
2025-04-22 14:03:24 +0200

    Add files via upload

---
+++ grafy/implementacje/dijkstra.py
@@ -0,0 +1,42 @@
+from math import inf
+from queue import PriorityQueue
+
+def dijkstra(s,G):
+    visited = [False for i in range (len(G))]
+    d = [inf for i in range (len(G))]
+    parent = [None for i in range (len(G))]
+    
+    Q = PriorityQueue()
+    d[s] = 0
+    Q.put((d[s],s))
+
+
+    while not Q.empty():
+        (dist,v) = Q.get()
+        if dist > d[v]:
+            continue
+
+        visited[v] = True
+        for u in G[v]:
+            if(d[v] + u[1] < d[u[0]]):
+                d[u[0]] = d[v] + u[1]
+                parent[u[0]] = v
+                Q.put((d[u[0]], u[0]))
+
+    return d
+
+
+G = [
+    [(1, 3), (6, 2)],
+    [(0, 3), (2, 2), (8, 1)],
+    [(1, 2), (3, 5)],
+    [(4, 20), (2, 5), (8, 1)],
+    [(5, 8), (3, 20), (7, 2)],
+    [(6, 3), (7, 1), (4, 8)],
+    [(0, 2), (7, 1), (5, 3)],
+    [(6, 1), (5, 1), (8, 7), (4, 2)],
+    [(7, 7), (1, 1), (3, 1)]
+]
+
+d = dijkstra(0,G)
+print(d[7])
