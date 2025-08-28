0406f72e1e56aeeaf7dc81688d186dc6b6d333cc
juliet2112 <julia.jamorska@gmail.com>
2025-08-28 12:16:30 +0200

    edited

--- egzaminy/2324/egz2b-2324.py/egz2b.py
+++ egzaminy/2324/egz2b-2324.py/egz2b.py
@@ -1,34 +1,40 @@
+#O(mlogm) > O(mlogn)
+#Dijkstra z oznaczaniem stanów: tego po jakim torze aktualnie jedzie pociąg
+
 from egz2btesty import runtests
 from math import inf
 from heapq import heappop, heappush
 
 def switch(s1,s2):
-    if s1 == 1 and s2 == 1: return 5
-    if s1 == 0 and s2 == 0: return 10
+    if s1 == 2:
+        return 0
+    if s1 == 1 and s2 == 1: return 10
+    if s1 == 0 and s2 == 0: return 5
     return 20
 
 def dijkstra(G,s,t):
     n = len(G)
-    d = [[inf for _ in range (n)] for _ in range (2)]
-    #0 - 'I', 1 - 'P'
+    d = [[inf for _ in range (n)] for _ in range (3)]
+    #0 - 'I', 1 - 'P', 2 - start
     PQ = []
-    d[0][s] = 0
-    d[1][s] = 0
-    for u, w, s2 in G[s]:
-        d[s2][u] = w  
-        heappush(PQ,(w,u,s2))
+    d[2][s] = 0
+    heappush(PQ,(0,s,2))
+
 
     while PQ:
         dist, v,s1 = heappop(PQ)
         if dist > d[s1][v]: continue
 
+        if v == t:
+            return dist
+
         for u,w,s2 in G[v]:
-            way = w + d[s1][v] + switch(s1,s2)
+            way = w + dist + switch(s1,s2)
             if d[s2][u] > way:
                 d[s2][u] = way
                 heappush(PQ,(d[s2][u],u,s2))
 
-    return min(d[0][t], d[1][t])
+    return min(d[0][t], d[1][t], d[2][t])
 
 def create_graph(G):
     n = 0
